9d0c759c868381dacee59ab8e8fde675430d1969
François Rongère <Francois.Rongere@ec-nantes.fr>
2018-04-03 16:56:19 +0200

    WIP

--- main2.cpp
+++ main2.cpp
@@ -6,139 +6,88 @@
 #include <vector>
 #include <iostream>
 
-
-template <class T>
-class Top;
-
-
-class VisitorBase {
-private:
-
-public:
-    template <class T>
-    void Apply(Top<T>& top) {
-        std::cout << top.GetData() << "\n";
-    }
-};
-
-
-
-
-//class VisitorGeneric {
-//
-//public:
-//    template <class U>
-//    void Apply(U* obj) {
-//
-//    }
-//};
-
-
-
-
-class Base {
-
-public:
-    virtual void Accept(VisitorBase& visitor) = 0;
-
-
-
-
-};
-
-template <class T>
-class Coucou : public Base {
-
-private:
-    T* m_data;
-
-public:
-    explicit Coucou(T* data) : m_data(data) {}
-
-    void Accept(VisitorBase& visitor) override {
-//        visitor.Apply(this);
-    }
-
-
-};
-
-class ContainerCoucou {
-private:
-    std::vector<std::unique_ptr<Base>> m_vector;
-
-public:
-    template <class T>
-    void Add(T* val) {
-        m_vector.emplace_back(std::make_unique<Coucou<T>>(val));
-    }
-
-};
-
-
-
-template <class U>
-class BaseInterface : public Base {
-
-public:
-//    void Interface() {
-//        static_cast<U*>(this)->Implementation();
-//    }
-
+struct MyDT {
+    int i = 1;
+    double d = 2;
 };
 
 
 
 template <class T>
-class Top : public BaseInterface<Top<T>> {
+class AbstractNode;
 
-private:
-    T* m_data;
+class Container;
+
+
+class Visitor {
 
 public:
 
-    explicit Top(T* val) : m_data(val) {};
+    void visit(const AbstractNode<int>& top) const;
 
-    void Implementation() {
-        std::cout << "Top";
-    }
+    void visit(const AbstractNode<double>& top) const;
 
-    T GetData() const {
-        return *m_data;
-    }
+    void visit(const AbstractNode<MyDT>& top) const;
 
-    void Accept(VisitorBase& visitor) override {
-        visitor.Apply(*this);
-    }
+    void visit(const AbstractNode<Container>& cont) const;
+
+
 };
+
+
+
+class Visitable {  // Visitable
+
+public:
+    virtual void Accept(const Visitor& visitor) const = 0;
+
+
+};
+
+
+template <class T>
+class AbstractNode : public Visitable {
+
+private:
+    T* m_data = nullptr;
+
+public:
+    AbstractNode() {}
+    explicit AbstractNode(T* data) : m_data(data) {}
+
+
+    void Accept(const Visitor& visitor) const override {
+        visitor.visit(*this);
+    }
+
+    const T* GetData() const {
+        return m_data;
+    }
+
+
+};
+
+class IntNode : public AbstractNode<IntNode> {};
 
 
 
 class Container {
 private:
-    typedef std::vector<std::unique_ptr<Base>> VectorType;
+    typedef std::vector<std::unique_ptr<Visitable>> VectorType;
     VectorType m_vector;
 
 public:
     template <class T>
     void Add(T* val) {
-        m_vector.emplace_back(std::make_unique<Top<T>>(val));
+        m_vector.emplace_back(std::make_unique<AbstractNode<T>>(val));
     }
 
-//    void Accept(VisitorBase& visitor) {
-//        for (const auto& e : m_vector) {
-//            visitor.Apply()
-//        }
-//    }
-
-
-
-    void Print() {
-
-        VisitorBase visitor;
+    void visit(const Visitor& visitor) const {
         for (const auto& e : m_vector) {
             e->Accept(visitor);
         }
     }
+
 
     typedef VectorType::iterator iterator;
     iterator begin() { return m_vector.begin(); }
@@ -149,7 +98,23 @@ public:
 
 };
 
+void Visitor::visit(const AbstractNode<int> &top) const {
+    std::cout << "I am integer with value " << *top.GetData() << std::endl;
+}
 
+void Visitor::visit(const AbstractNode<double> &top) const {
+    std::cout << "I am double with value " << *top.GetData() << std::endl;
+}
+
+void Visitor::visit(const AbstractNode<MyDT> &top) const {
+    std::cout << "I am DT with val "<< top.GetData()->i << " and " << top.GetData()->d << "\n";
+}
+
+void Visitor::visit(const AbstractNode<Container> &cont) const {
+    std::cout << "I am a container and I am going to visit myself\n";
+    cont.GetData()->visit(*this);
+
+}
 
 
 
@@ -157,6 +122,7 @@ int main() {
 
     int i = 2;
     double d = 3.148769876986;
+    MyDT dt;
 
 
     Container c;
@@ -164,16 +130,32 @@ int main() {
     c.Add<int>(&i);
     c.Add<double>(&d);
 
-    c.Print();
+    c.Add(&dt);
 
-    for (const auto& e : c) {
-
-    }
-
+//    c.Print();
+    Visitor visitor;
 
 
-    ContainerCoucou c2;
-    c2.Add<int>(&i);
+    c.visit(visitor);
+
+
+
+    Container c2;
+    int i2 = 10;
+    double d2 = 1.414;
+    c2.Add<int>(&i2);
+    c2.Add<double>(&d2);
+
+    c.Add<Container>(&c2);
+
+    c.visit(visitor);
+
+
+//    for (const auto& e : c) {
+//        e->Accept(visitor);
+//    }
+
+
 
 
 
