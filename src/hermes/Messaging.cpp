644461b9d081daab39a26bcea37424ea9dfc39a7
François Rongère <francois.rongere@dice-engineering.com>
2021-04-19 21:22:07 +0200

    Applying official source code formatting

--- src/hermes/Messaging.cpp
+++ src/hermes/Messaging.cpp
@@ -13,15 +13,15 @@
 
 namespace hermes {
 
-    CSVSerializer *Message::AddCSVSerializer(std::string CSVFile) {
-        auto *csvSerializer = AddSerializer(new CSVSerializer(CSVFile));
-        return dynamic_cast<CSVSerializer *>(csvSerializer);
-    }
+  CSVSerializer *Message::AddCSVSerializer(std::string CSVFile) {
+    auto *csvSerializer = AddSerializer(new CSVSerializer(CSVFile));
+    return dynamic_cast<CSVSerializer *>(csvSerializer);
+  }
 
-    PrintSerializer *Message::AddPrintSerializer() {
-        auto *printSerializer = AddSerializer(new PrintSerializer());
-        return dynamic_cast<PrintSerializer *>(printSerializer);
-    }
+  PrintSerializer *Message::AddPrintSerializer() {
+    auto *printSerializer = AddSerializer(new PrintSerializer());
+    return dynamic_cast<PrintSerializer *>(printSerializer);
+  }
 
 
 }  // end namespace hermes
